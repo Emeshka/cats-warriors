f396fd9aa7f48fbe915007987e1ac8c8dd4e1a0d
30028555 <alexa.emelina@gmail.com>
2020-05-21 12:26:56 +0300

    console_launcher.exe, удалены старые файлы

--- мусорка/console_launcher.bat
+++ мусорка/console_launcher.bat
@@ -1,7 +1,7 @@
 ::[Bat To Exe Converter]
 ::
 ::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFEgEHFbaaT7vOpwY7enL/eWNp04JR94ceZvI7bWcMukc+FbVYJM+wkbqRHrvvfrooKNL3tWEeVJLtWuLec6fvG8=
+::fBw5plQjdCuDJAi78E0zPRZRQhCRAGi7EqZR/+np/eOIsF4h5D6Omz46amo/wFDJWpXYfJUi2DRTm8Rs
 ::YAwzuBVtJxjWCl3EqQJgSA==
 ::ZR4luwNxJguZRRnk
 ::Yhs/ulQjdF+5
@@ -9,30 +9,36 @@
 ::cBs/ulQjdF+5
 ::ZR41oxFsdFKZSDk=
 ::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpSI=
+::cRo6pxp7LAbNWATEpCI=
 ::egkzugNsPRvcWATEpCI=
 ::dAsiuh18IRvcCxnZtBJQ
 ::cRYluBh/LU+EWAnk
 ::YxY4rhs+aU+IeA==
 ::cxY6rQJ7JhzQF1fEqQJQ
 ::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQIHKQlGRQuQH2quFfUt/e3o+4o=
+::ZQ05rAF9IAHYFVzEqQIHKQlGRQuQfEi7EqZc3O3j+6qmplsYWvYqaoq7
 ::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDDODLnmzCadcy+nv/Kq2t0gOQKIzeZrV2byLMoA=
-::cRolqwZ3JBvQF1fEqQIRaA9RVBDCPW+sA7sI/fr+r+OJ4lkVUaIIeZ3J07ucYMMS/lakYpk/2n4U+A==
+::fBEirQZwNQPfEVWB+kM9LVsJDDODLnmzCadcy+nv/Kqzp1UJFMM7borVzqGcJYA=
+::cRolqwZ3JBvQF1fEqQIHKQlGRQuQfEi7EqZc3O3j+6qmplsYWvYqaoq7
 ::dhA7uBVwLU+EWDk=
 ::YQ03rBFzNR3SWATElA==
 ::dhAmsQZ3MwfNWATElA==
 ::ZQ0/vhVqMQ3MEVWAtB9wSA==
 ::Zg8zqx1/OA3MEVWAtB9wSA==
 ::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFEgEHFbaaT7vOpwY7enL/eWNp04JR94ceZvI7bWcMukc+FbVYJM+wkZWmd4CDxNdbC29PEExsWsi
+::Zh4grVQjdCuDJAi78E0zPRZRQhCRAGi7EqZR/+np/eOIsF4hV+0xa4DX34uCIfUd6U3hZ9gozn86
 ::YB416Ek+ZG8=
 ::
 ::
 ::978f952a14a936cc963da21a135fa983
 @echo off
 chcp 65001
-java -jar "./out/artifacts/CatsWarriorsQuest_jar/CatsWarriorsQuest.jar"
+cd "warriorCats-win32-ia32"
+if exist "warriorCats.exe" (
+	del "warriorCats.exe"
+)
+"../unzip.exe" "warriorCats.zip"
+"./warriorCats.exe"
 echo "Если игра завершилась с ошибками, или ты видишь ошибки тут, в консоли, скопируй текст ошибки выше и отправь мне. Иначе закрой это окно."
 pause
+cd ..
--- мусорка/start_game_windows.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-java -jar "./out/artifacts/CatsWarriorsQuest_jar/CatsWarriorsQuest.jar"
-echo "Если игра завершилась без ошибок, закрой это окно. Иначе скопируй текст ошибки выше и отправь мне вк"
-pause
