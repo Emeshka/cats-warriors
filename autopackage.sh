d2a8246ebb0ce856b0a0a1a61d2c8e1db5b5c4a3
30028555 <alexa.emelina@gmail.com>
2020-05-21 11:19:44 +0300

    Автоупаковщик удаляет исполняемые файлы после создания архива

--- autopackage.sh
+++ autopackage.sh
@@ -1,10 +1,12 @@
 rm -r "warriorCats-win32-ia32"
-electron-packager . warriorCats --platform=win32 --arch=ia32 --ignore="(icon.icns|autopackage.sh|console_launcher.sh|console_launcher.bat|мусорка)"
+electron-packager . warriorCats --platform=win32 --arch=ia32 --ignore="(autopackage.sh|console_launcher.sh|console_launcher.bat|мусорка|zip.exe|unzip.exe)"
 cd "warriorCats-win32-ia32"
 ../zip.exe "warriorCats.zip" "warriorCats.exe"
+rm "warriorCats.exe"
 cd ..
 rm -r "warriorCats-linux-ia32"
-electron-packager . warriorCats --platform=linux --arch=ia32 --ignore="(icon.icns|autopackage.sh|console_launcher.sh|console_launcher.bat|мусорка)"
+electron-packager . warriorCats --platform=linux --arch=ia32 --ignore="(autopackage.sh|console_launcher.sh|console_launcher.bat|мусорка|zip.exe|unzip.exe)"
 cd "warriorCats-linux-ia32"
-../zip.exe "warriorCats.zip" "warriorCats.exe"
+../zip.exe "warriorCats.zip" "warriorCats"
+rm "warriorCats"
 cd ..
