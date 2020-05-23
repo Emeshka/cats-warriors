a5c5cd1efc39fe2301d4ab11de1bc084ef5e4a45
30028555 <alexa.emelina@gmail.com>
2020-05-24 02:10:17 +0300

    Обновлены запакованные версии.

--- warriorCats-linux-ia32/resources/app/README.md
+++ warriorCats-linux-ia32/resources/app/README.md
@@ -10,23 +10,25 @@ Stage of development: zero, the creation of the most basic functions.
 If you came here to download and play, it's currently impossible. But if you can help us with testing and developing, or with image drawing - I appreciate any help!
 
 ### System requirements:
-	1. ~200 MB of free space
-	2. Tested on following operating systems:
-		Windows 10 Home 64-bit
-	3. Should work on any version of Windows or Linux. Needs to be tested.
+1. ~200 MB of free space
+2. Tested on following operating systems:
+	Windows 10 Home 64-bit
+3. Should work on any version of Windows or Linux. Needs to be tested.
 
 ## To install the game:
 While it's still developing, to install it follow these steps:
-	1. Download this folder (Clone or Download > Download ZIP).
-	2. Unpack ZIP archive to some folder. Open that folder.
-	3. For Windows: copy 'warriorCats-win32-ia32' folder and the file 'console_launcher.bat' to your desired location
-	4. For Linux: copy 'warriorCats-linux-ia32' folder and the file 'console_launcher.sh' to your desired location
-	5. You can now delete the other unpacked files and folders, they are unnecessary.
+
+1. Download this folder (Clone or Download > Download ZIP).
+2. Unpack ZIP archive to some folder. Open that folder.
+3. For Windows: copy 'warriorCats-win32-ia32' folder and the file 'console_launcher.bat' to your desired location.
+4. For Linux: copy 'warriorCats-linux-ia32' folder and the file 'console_launcher.sh' to your desired location.
+5. You can now delete the other unpacked files and folders, they are unnecessary.
+
 After the release the installation will be much simpler :)
 
 ## To run the game:
-	1. Windows: double-click file console_launcher.bat
-	2. Linux: double-click file console_launcher.sh
+1. Windows: double-click file console_launcher.exe
+2. Linux: double-click file console_launcher.sh
 
 # Текстовый квест по Котам-Воителям
 
@@ -37,23 +39,25 @@ After the release the installation will be much simpler :)
 Если вы пришли скачать и поиграть, пока этого сделать не получится. Но если вы можете помочь нам в тестировании и разработке, или в рисовании иллюстраций - будем очень признательны!
 
 ### Системные требования:
-	1. ~200 МБ свободного места
-	2. Тестировалось на операционных системах:
-		Windows 10 Домашняя 64-bit
-	3. Должно работать на любой версии Windows или Linux. Необходимо тестирование.
+1. ~200 МБ свободного места
+2. Тестировалось на операционных системах:
+	Windows 10 Домашняя 64-bit
+3. Должно работать на любой версии Windows или Linux. Необходимо тестирование.
 
 ### Для установки игры
 Пока идет разработка, установка будет не очень удобной:
-	1. Скачайте эту папку (Clone or Download > Download ZIP).
-	2. Распакуйте архив в выбранную папку. Откройте эту папку.
-	3. Для Windows: скопируйте папку 'warriorCats-win32-ia32' и файл 'console_launcher.bat' в удобное для вас место.
-	4. Для Linux: скопируйте папку 'warriorCats-linux-ia32' и файл 'console_launcher.sh' в удобное для вас место.
-	5. Теперь вы можете удалить другие распакованные из архива файлы и папки, они больше не нужны.
+
+1. Скачайте эту папку (Clone or Download > Download ZIP).
+2. Распакуйте архив в выбранную папку. Откройте эту папку.
+3. Для Windows: скопируйте папку 'warriorCats-win32-ia32' и файл 'console_launcher.bat' в удобное для вас место.
+4. Для Linux: скопируйте папку 'warriorCats-linux-ia32' и файл 'console_launcher.sh' в удобное для вас место.
+5. Теперь вы можете удалить другие распакованные из архива файлы и папки, они больше не нужны.
+
 После релиза установка станет намного проще :)
 
 ### Для запуска игры:
-	1. Windows: запустите двойным щелчком файл console_launcher.bat
-	2. Linux: запустите двойным щелчком файл console_launcher.sh
+1. Windows: запустите двойным щелчком файл console_launcher.exe
+2. Linux: запустите двойным щелчком файл console_launcher.sh
 
 ## Лицензия:
 
--- warriorCats-win32-ia32/resources/app/renderer.js
+++ warriorCats-win32-ia32/resources/app/renderer.js
@@ -9,10 +9,13 @@ log = console.log
 
 const fs = window.require('fs');
 const path = require('path')
-const s = require(path.join(__dirname, 'strings.js'));
-//log(s.s);
+const strings = require(path.join(__dirname, 'strings.js'));
 
-document.title = s.s['app_name']
+function _(key) {
+    return strings.s[key] ? strings.s[key] : key;
+}
+
+document.title = _('app_name')
 
 const sound = {};
 sound.ids = {}
@@ -52,9 +55,17 @@ sound.play = function(name) {
     }
 }
 
+/* ------------------------------------------------------------------------------- */
+
 var loadedGame = null
 
-function button(text, onclick, width, height) {
+function startNewGame(params) {
+    //
+}
+
+/* ------------------------------------------------------------------------------- */
+
+function createButton(text, onclick, width, height) {
     var button = document.createElement("input")
     button.setAttribute('type', 'button')
     button.onclick = onclick;
@@ -67,7 +78,7 @@ function button(text, onclick, width, height) {
     return button;
 }
 
-function spacer() {
+function createSpacer() {
     var div = document.createElement('div')
     div.style.display = 'inline-block'
     div.style.verticalAlign = 'middle'
@@ -75,8 +86,79 @@ function spacer() {
     return div
 }
 
-function receiveLoadedGame(jGame) {
-    loadedGame = JSON.parse(jGame)
+// функция, рекурсивно превращающая иерархически структурированный объект data в DOM Element в виде ПДА
+function createPdaElementFromData(data) {
+    let div = document.createElement('div')
+    let leftColumn = document.createElement('div')
+    leftColumn.className = 'pda_left_column'
+    let rightColumn = document.createElement('div')
+    rightColumn.className = 'pda_right_column'
+
+    function articleRecordLink(text) {
+        let entry = document.createElement("div")
+        entry.innerHTML = text;
+        entry.className = 'pda_left_column_entry'
+        return entry;
+    }
+
+    function articleFolder(text) {
+        let entry = document.createElement("div")
+        entry.innerHTML = text;
+        entry.className = 'pda_left_column_entry pda_left_column_entry_class'
+        return entry;
+    }
+
+    var selectedEntry = null;
+    function parseLevel(data, leftColumnContainer) {
+        for (let key in data) {
+            let value = data[key]
+            if (value == 1) {//простая статья в пда
+                let articleLink = articleRecordLink(_(key+'_pda_record'))
+                articleLink.onclick = function() {
+                    rightColumn.innerHTML = _(key+'_pda_article')
+                    this.className = 'pda_left_column_entry pda_left_column_entry_selected';
+                    if (selectedEntry && selectedEntry != this) selectedEntry.className = 'pda_left_column_entry';
+                    selectedEntry = this;
+                };
+                leftColumnContainer.appendChild(articleLink)
+            } else {//папка в пда
+                let articleFolderToggle = articleFolder(_(key+'_pda_folder'))
+                let invisContainer = document.createElement('div')
+                invisContainer.style.display = 'none';
+                invisContainer.className = 'pda_left_column_class_childnodes_block';
+                parseLevel(value, invisContainer)
+                articleFolderToggle.onclick = function() {
+                    if (invisContainer.style.display == 'none') {
+                        invisContainer.style.display = 'block';
+                        this.className += ' pda_left_column_entry_class_opened'
+                    } else {
+                        invisContainer.style.display = 'none';
+                        this.className = 'pda_left_column_entry pda_left_column_entry_class'
+                    }
+                }
+                leftColumnContainer.appendChild(articleFolderToggle)
+                leftColumnContainer.appendChild(invisContainer)
+            }
+        }
+    }
+    parseLevel(data, leftColumn)
+    div.appendChild(leftColumn);
+    div.appendChild(rightColumn);
+    return div;
+}
+
+// загрузочный экран
+function showLoadingGame() {
+    let con = document.body;
+    con.style.backgroundImage = "url('textures/main_view_bg.jpg')"
+    let loading = document.createElement('div')
+    loading.innerHTML = _('loading')
+    loading.className = 'game_view_block'
+    loading.style.position = 'absolute';
+    loading.style.bottom = '10px';
+    loading.style.left = '10px';
+    loading.style.width = 'calc(100vw - 40px)'
+    con.appendChild(loading)
 }
 
 function mainMenu() {
@@ -84,7 +166,7 @@ function mainMenu() {
     con.style.backgroundImage = "url('textures/main_menu.png')"
     var mm = document.createElement('div');
     mm.id = "main_menu"
-    mm.appendChild(spacer())
+    mm.appendChild(createSpacer())
 
     function savedGameEntry() {
         let div = document.createElement('div')
@@ -92,31 +174,52 @@ function mainMenu() {
         return div;
     }
 
+    function createHeader(text) {
+        let header = document.createElement("span")
+        header.innerHTML = text
+        header.className = 'main_menu_header'
+        return header
+    }
+
     var list = document.createElement('ul');
     list.className = "left_main_menu_panel"
-    var newGameParams = null, newGameStart = null, loadGameList = null;
-    var mainOptions = ['new_game', 'load', 'exit'];
+    var newGameBlock = null, loadGameList = null, encycBlock = null, credits = null;
+    var mainOptions = ['new_game', 'load', 'encyclopedy', 'credits', 'exit'];
     var onclicks = [
         function () {
-            if (newGameParams || newGameStart) return;
-            if (loadGameList) loadGameList.remove();
+            if (newGameBlock) return;
+            if (loadGameList) {
+                loadGameList.remove();
+                loadGameList = null;
+            }
+            if (credits) {
+                credits.remove();
+                credits = null;
+            }
+            if (encycBlock) {
+                encycBlock.remove();
+                encycBlock = null;
+            }
 
-            newGameParams = document.createElement('div')
-            newGameParams.className = "right_main_menu_panel"
+            newGameBlock = document.createElement('div')
+            newGameBlock.className = "game_view_block right_main_menu_panel"
+            let buttonStartHeight = 30;
+            var newGameParams = document.createElement('div')
+            newGameParams.className = 'main_menu_block_under_header'
+            newGameParams.style.maxHeight = 'calc(100vh - 60px - 50px - '+buttonStartHeight+'px)';
             var difComment = document.createElement('p')
-            difComment.innerHTML = s.s['difficulty_level']
+            difComment.innerHTML = _('difficulty_level_1_descr')
             var difficulty = document.createElement('select')
             difficulty.onchange = function() {
-                difComment.innerHTML = s.s[this.value+'_descr']
+                difComment.innerHTML = _(this.value+'_descr')
             }
-            var opts = ['none', 'difficulty_level_1', 'difficulty_level_2', 'difficulty_level_3', 'difficulty_level_4']
+            let opts = ['difficulty_level_1', 'difficulty_level_2', 'difficulty_level_3', 'difficulty_level_4']
             for (let i = 0; i < opts.length; i++) {
-                var o = opts[i]
+                let o = opts[i]
                 var option = document.createElement('option')
                 option.value = o
-                option.innerHTML = s.s[o]
-                if (o == 'none') {
-                    option.setAttribute('disabled', 'disabled')
+                option.innerHTML = _(o)
+                if (o == 'difficulty_level_1') {
                     option.setAttribute('selected', 'selected')
                 }
                 difficulty.appendChild(option)
@@ -124,10 +227,10 @@ function mainMenu() {
             newGameParams.appendChild(difficulty)
             newGameParams.appendChild(difComment)
 
-            var eras = ['old', 'new']
+            let eras = ['old', 'new']
             var selectedEra = ''
             var eraComment = document.createElement('p')
-            eraComment.innerHTML = s.s['era_choice']
+            eraComment.innerHTML = _('era_choice')
             for (let i = 0; i<eras.length; i++) {
                 let e = eras[i]
                 var era = document.createElement('input')
@@ -137,11 +240,11 @@ function mainMenu() {
                 if (i == 0) era.checked = true
                 var eraLabel = document.createElement('label')
                 eraLabel.appendChild(era)
-                eraLabel.innerHTML += s.s[e+'_era']
+                eraLabel.innerHTML += _(e+'_era')
                 eraLabel.onclick = function() {
                     if (selectedEra != e) {
                         selectedEra = e
-                        eraComment.innerHTML = s.s[e+'_era_descr']
+                        eraComment.innerHTML = _(e+'_era_descr')
                     }
                 }
                 newGameParams.appendChild(eraLabel)
@@ -150,15 +253,15 @@ function mainMenu() {
 
             var table = document.createElement('table')
             table.id = 'race_choice'
-            var cells = [
+            let cells = [
                 ['swamp', 'wind'],
                 ['forest', 'mist']
             ];
             var selectedRace = ''
             var raceName = document.createElement('h3')
-            raceName.innerHTML = s.s['none']
+            raceName.innerHTML = _('none')
             var raceComment = document.createElement('p')
-            raceComment.innerHTML = s.s['race_choice']
+            raceComment.innerHTML = _('race_choice')
             for (let i = 0; i<cells.length; i++) {
                 var tr = document.createElement('tr')
                 for (let j = 0; j<cells[i].length; j++) {
@@ -172,8 +275,8 @@ function mainMenu() {
                             let oldRace = document.getElementById(selectedRace+'_td')
                             if (oldRace) oldRace.className = 'unselected_image_option'
                             selectedRace = race
-                            raceName.innerHTML = s.s[race+'_race']
-                            raceComment.innerHTML = s.s[race+'_race_descr']
+                            raceName.innerHTML = _(race+'_race')
+                            raceComment.innerHTML = _(race+'_race_descr')
                             this.className = 'selected_image_option unselected_image_option'
                         }
                     }
@@ -185,48 +288,146 @@ function mainMenu() {
             newGameParams.appendChild(raceName)
             newGameParams.appendChild(raceComment)
 
-            newGameStart = document.createElement('div')
-            newGameStart.className = "right_main_menu_panel"
-            newGameStart.id = 'new_game_start'
-            newGameStart.innerHTML = s.s['start_new_game']
-            newGameStart.onclick = function() {
-                mm.remove()
-                showLoadingGame()
-                var params = {
+            var seasonComment = document.createElement('p')
+            seasonComment.innerHTML = _('summer_descr')
+            var season = document.createElement('select')
+            season.onchange = function() {
+                seasonComment.innerHTML = _(this.value+'_descr')
+            }
+            opts = ['winter', 'spring', 'summer', 'autumn']
+            for (let i = 0; i < opts.length; i++) {
+                let o = opts[i]
+                var option = document.createElement('option')
+                option.value = o
+                option.innerHTML = _(o)
+                if (o == 'summer') {
+                    option.setAttribute('selected', 'selected')
+                }
+                season.appendChild(option)
+            }
+            newGameParams.appendChild(season)
+            newGameParams.appendChild(seasonComment)
+
+            var newGameStart = document.createElement('div')
+            newGameStart.style.borderBottom = '1px solid white';
+            let header = createHeader(_('start_new_game_header'))
+            let button = createButton(_('start_new_game'), function() {
+                let params = {
                     difficulty: difficulty.value,
                     era: selectedEra,
                     race: selectedRace,
                     season: season.value
                 }
-                //java.startNewGame(JSON.stringify(params))
-            }
+                if (params.difficulty && params.era && params.race && params.season) {
+                    mm.remove()
+                    showLoadingGame()
+                    startNewGame(params)
+                } else {
+                    let warnings = _('warning_not_enough_data_new_game');
+                    if (!params.difficulty) warnings += '\n' + _('warning_enter_difficulty');
+                    if (!params.era) warnings += '\n' + _('warning_enter_era');
+                    if (!params.race) warnings += '\n' + _('warning_enter_race');
+                    if (!params.season) warnings += '\n' + _('warning_enter_season');
+                    alert(warnings);
+                }
+            }, null, buttonStartHeight+'px')
+            newGameStart.appendChild(header)
+            newGameStart.appendChild(button)
 
-            mm.appendChild(newGameParams)
-            mm.appendChild(newGameStart)
+            newGameBlock.appendChild(newGameStart)
+            newGameBlock.appendChild(newGameParams)
+            mm.appendChild(newGameBlock)
         },
         function () {
             if (loadGameList) return;
-            if (newGameParams || newGameStart) {
-                newGameParams.remove();
-                newGameStart.remove();
+            if (newGameBlock) {
+                newGameBlock.remove();
+                newGameBlock = null;
+            }
+            if (credits) {
+                credits.remove();
+                credits = null;
+            }
+            if (encycBlock) {
+                encycBlock.remove();
+                encycBlock = null;
             }
 
             loadGameList = document.createElement('div')
-            loadGameList.className = "right_main_menu_panel"
-            /*var gamesJArray = java.loadGameList()
-            var gamesArray = gamesJArray ? JSON.parse(gamesJArray) : "null"
-            if (gamesArray) {
-                for (var i = 0; i<gamesArray.length; i++) {
-                    //
-                }
-            }*/
+            loadGameList.className = "right_main_menu_panel game_view_block"
             mm.appendChild(loadGameList)
         },
-        function () {/*java.exit()*/}
+        function () {
+            if (encycBlock) return;
+            if (newGameBlock) {
+                newGameBlock.remove();
+                newGameBlock = null;
+            }
+            if (loadGameList) {
+                loadGameList.remove();
+                loadGameList = null;
+            }
+            if (credits) {
+                credits.remove();
+                credits = null;
+            }
+
+            encycBlock = document.createElement('div')
+            encycBlock.className = "right_main_menu_panel game_view_block"
+
+            let header = createHeader(_('encyclopedy_header'))
+            let articleStructure = {
+                'interface': {'main_menu_interface':1, 'game_interface':1},
+                'world': {'basic_warrior_cats_society':1, 'social_structure':1, 'eras':1,
+                        'maps':1, 'groupings':1, 'enemies_and_dangers':1},
+                'gameplay': {'basic_gameplay':1, 'upgrading_character':1, 'quests':1, 'pda':1,
+                        'lifesigns':1, 'reputation':1, 'status':1, 'dialog_history':1, 'map':1}
+            };
+            let pda = createPdaElementFromData(articleStructure)
+            encycBlock.appendChild(header)
+            encycBlock.appendChild(pda)
+            mm.appendChild(encycBlock)
+        },
+        function () {
+            if (credits) return;
+            if (newGameBlock) {
+                newGameBlock.remove();
+                newGameBlock = null;
+            }
+            if (loadGameList) {
+                loadGameList.remove();
+                loadGameList = null;
+            }
+            if (encycBlock) {
+                encycBlock.remove();
+                encycBlock = null;
+            }
+
+            credits = document.createElement('div')
+            credits.className = "right_main_menu_panel game_view_block"
+            let header = createHeader(_('credits_header'))
+            credits.appendChild(header)
+            var text = document.createElement('div')
+            text.className = 'main_menu_block_under_header'
+            fs.readFile(path.join(__dirname, 'CREDITS.txt'), 'utf8', function(err, contents) {
+                if (err) text.innerHTML = err;
+                else text.innerHTML = contents.replace(/\n/g, "<br>");
+            });
+            credits.appendChild(text)
+            mm.appendChild(credits)
+        },
+        function () {
+            let sure = confirm(_('sure_to_exit'))
+            if (sure) {
+                const remote = require('electron').remote
+                let w = remote.getCurrentWindow()
+                w.close()
+            }
+        }
     ];
     for (let i = 0; i<mainOptions.length; i++) {
-        var li = document.createElement('li');
-        li.appendChild(button(s.s[mainOptions[i]], onclicks[i]))
+        let li = document.createElement('li');
+        li.appendChild(createButton(_(mainOptions[i]), onclicks[i]))
         list.appendChild(li)
     }
     mm.appendChild(list)
