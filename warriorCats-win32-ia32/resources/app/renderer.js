fbdab8f124fb6995afbb90e85747c301b1ae0e89
30028555 <alexa.emelina@gmail.com>
2020-05-24 21:08:33 +0300

    Главное меню, кроме кнопки сохранить.

--- warriorCats-win32-ia32/resources/app/renderer.js
+++ warriorCats-win32-ia32/resources/app/renderer.js
@@ -17,11 +17,19 @@ function _(key) {
 
 document.title = _('app_name')
 
+function quit() {
+    const remote = require('electron').remote
+    let w = remote.getCurrentWindow()
+    w.close()
+}
+
 const sound = {};
 sound.ids = {}
 fs.readdir(path.join(__dirname, 'sound'), function(err, files) {
     if (err) {
-        return console.log('Unable to scan directory: ' + err);
+        console.log('Unable to scan directory: ' + err);
+        alert(_('error_scan_sounds'))
+        quit()
     } 
     files.forEach(function(file) {
         let fileName = file.substring(0, file.length - 4)
@@ -57,10 +65,40 @@ sound.play = function(name) {
 
 /* ------------------------------------------------------------------------------- */
 
-var loadedGame = null
+var world = null
 
 function startNewGame(params) {
     //
+}
+
+// First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/%(\d+)%/g, function(match, number) {
+            return typeof args[number] != 'undefined' ? '<span class="template_variable_emphasis">'+args[number]+"</span>" : match;
+        });
+    };
+}
+
+function printDate(date) {
+    let months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+    let nth = _('nth_of_' + months[date.getMonth()]).format(date.getDate());
+    let time = _('standard_time_format').format(date.getHours(), date.getMinutes(), date.getSeconds())
+    return _('standard_date_format').format(date.getFullYear(), nth, time);
+}
+
+function prettyPrintAge(birthDate, currentDate) {
+    let monthsAmount = (currentDate.getTime() - birthDate.getTime()) / (1000*60*60*24*30)
+    let str = '';
+    if (monthsAmount >= 12) {
+        let yearsAmount = monthsAmount / 12
+        monthsAmount = monthsAmount % 12
+        str = _('age_n_years_m_months').format(Math.floor(yearsAmount), Math.floor(monthsAmount))
+    } else {
+        str = _('age_n_months').format(Math.floor(monthsAmount))
+    }
+    return str
 }
 
 /* ------------------------------------------------------------------------------- */
@@ -115,7 +153,7 @@ function createPdaElementFromData(data) {
             if (value == 1) {//простая статья в пда
                 let articleLink = articleRecordLink(_(key+'_pda_record'))
                 articleLink.onclick = function() {
-                    rightColumn.innerHTML = _(key+'_pda_article')
+                    rightColumn.innerHTML = _(key+'_pda_article').replace(/\n/g, "<br>")
                     this.className = 'pda_left_column_entry pda_left_column_entry_selected';
                     if (selectedEntry && selectedEntry != this) selectedEntry.className = 'pda_left_column_entry';
                     selectedEntry = this;
@@ -168,10 +206,129 @@ function mainMenu() {
     mm.id = "main_menu"
     mm.appendChild(createSpacer())
 
-    function savedGameEntry() {
+    var selectedSavedGame = null;
+    function savedGameEntry(filename, game, entryOnclick) {
         let div = document.createElement('div')
-        
+        div.className = 'saved_game_entry'
+
+        let leftColumn = document.createElement('div')
+        leftColumn.className = 'saved_game_info_part'
+        let rightColumn = document.createElement('div')
+        rightColumn.className = 'saved_game_img_part'
+
+        if (game) {
+            let gameName = filename.substring(0, filename.length - 5);
+            let nameBlock = document.createElement('div');
+            nameBlock.className = 'saved_game_name';
+            nameBlock.innerHTML = gameName;
+            let savedTimestamp = document.createElement('div');
+            savedTimestamp.className = 'saved_game_timestamp';
+            savedTimestamp.innerHTML = printDate(new Date(game.savedTimestamp));
+            nameBlock.appendChild(savedTimestamp)
+            nameBlock.title = _('saved_game_name_block_title').format(gameName, savedTimestamp.innerHTML);
+
+            let actorBlock = document.createElement('div');
+            actorBlock.className = 'saved_game_actor';
+            let gender = document.createElement('img');
+            gender.className = 'saved_game_gender';
+            gender.src = "textures/"+game.actor.gender+"_icon.png";
+            let actorName = document.createElement('div');
+            actorName.className = 'saved_game_actor_name';
+            actorName.innerHTML = game.actor.name;
+            let age = document.createElement('div');
+            age.className = 'saved_game_age';
+            age.innerHTML = prettyPrintAge(new Date(game.actor.birthDate), new Date(game.date));
+            let gameDate = document.createElement('div');
+            gameDate.className = 'saved_game_date';
+            gameDate.innerHTML = printDate(new Date(game.date));
+            actorBlock.appendChild(gender)
+            actorBlock.appendChild(actorName)
+            actorBlock.appendChild(age)
+            actorBlock.appendChild(gameDate)
+            actorBlock.title = _('saved_game_actor_block_title').format(game.actor.name, age.innerHTML, gameDate.innerHTML);
+
+            leftColumn.appendChild(nameBlock)
+            leftColumn.appendChild(actorBlock)
+
+            rightColumn.style.backgroundImage = "url('"+game.savedActivityBg+"')";
+        } else {
+            leftColumn.innerHTML = filename;
+            rightColumn.style.backgroundImage = "url('textures/invalid_saved_game.gif')";
+        }
+
+        div.appendChild(leftColumn)
+        div.appendChild(rightColumn)
+        div.onclick = function() {
+            this.className = 'saved_game_entry saved_game_entry_selected';
+            if (selectedSavedGame && selectedSavedGame != this) selectedSavedGame.className = 'saved_game_entry';
+            selectedSavedGame = this;
+            entryOnclick()
+        }
         return div;
+    }
+
+    function emptySavedEntry() {
+        let div = document.createElement('div')
+        div.className = 'saved_game_entry saved_game_entry_empty'
+        div.innerHTML = _('saves_folder_is_empty')
+        return div
+    }
+
+    function createSavedGameList(entryOnclick, invalidEntryOnclick) {
+        let gameList = document.createElement('div')
+        gameList.className = 'main_menu_block_under_header'
+        gameList.style.maxHeight = 'calc(100vh - 60px - 50px - 30px)';
+        let savesFolder = path.join(__dirname, 'saves')
+        if (fs.existsSync(savesFolder)) {
+            fs.readdir(savesFolder, {withFileTypes: true}, function(err, files) {
+                if (err) {
+                    console.log('Unable to scan directory: ' + err)
+                    gameList.innerHTML = _('error_scan_saves_directory');
+                    return
+                }
+                if (!files.length) {
+                    gameList.appendChild(emptySavedEntry())
+                } else {
+                    files.forEach(function(file) {
+                        if (!file.isDirectory() && file.name.endsWith(".json")) {
+                            let filepath = savesFolder + "/" + file.name;
+                            fs.readFile(filepath, 'utf-8', function(readFileErr, content) {
+                                let entry = null, object = {}, validJson = true;
+                                try {
+                                    object = JSON.parse(content)
+                                } catch (parseError) {
+                                    console.log(parseError)
+                                    validJson = false
+                                }
+                                let validGame = ('savedTimestamp' in object) && ('date' in object) && ('actor' in object)
+                                        && ('sublocation' in object) && ('savedActivityBg' in object)
+                                if (readFileErr || !validJson || !validGame) {
+                                    console.log('Unable to read saved game: readFileErr=' + readFileErr + ', validJson=' + validJson + ', validGame=' + validGame)
+                                    entry = savedGameEntry(_('error_read_saved_game'), null, invalidEntryOnclick)
+                                    gameList.appendChild(entry)
+                                } else {
+                                    entry = savedGameEntry(file.name, object, entryOnclick)
+                                }
+                                entry.dataset.path = filepath;
+                                gameList.appendChild(entry)
+                            });
+                        }
+                    });
+                }
+            });
+        } else {
+            fs.mkdir(savesFolder, {}, (err) => {
+                if (err) {
+                    console.log('Unable to create directory: ' + err)
+                    gameList.innerHTML = _('error_create_saves_directory');
+                    return
+                }
+                else {
+                    gameList.appendChild(emptySavedEntry())
+                }
+            });
+        }
+        return gameList
     }
 
     function createHeader(text) {
@@ -191,6 +348,7 @@ function mainMenu() {
             if (loadGameList) {
                 loadGameList.remove();
                 loadGameList = null;
+                selectedSavedGame = null;
             }
             if (credits) {
                 credits.remove();
@@ -207,6 +365,31 @@ function mainMenu() {
             var newGameParams = document.createElement('div')
             newGameParams.className = 'main_menu_block_under_header'
             newGameParams.style.maxHeight = 'calc(100vh - 60px - 50px - '+buttonStartHeight+'px)';
+
+            let genders = ['female', 'male']
+            var selectedGender = 'male'
+            var genderComment = document.createElement('p')
+            genderComment.innerHTML = _('male_comment')
+            for (let i = 0; i<genders.length; i++) {
+                let e = genders[i]
+                var gender = document.createElement('input')
+                gender.setAttribute('type', 'radio')
+                gender.setAttribute('name', 'gender')
+                gender.value = e
+                if (i == 1) gender.setAttribute('checked', 'checked')
+                var genderLabel = document.createElement('label')
+                genderLabel.appendChild(gender)
+                genderLabel.innerHTML += _(e)
+                genderLabel.onclick = function() {
+                    if (selectedGender != e) {
+                        selectedGender = e
+                        genderComment.innerHTML = _(e+'_comment')
+                    }
+                }
+                newGameParams.appendChild(genderLabel)
+            }
+            newGameParams.appendChild(genderComment)
+
             var difComment = document.createElement('p')
             difComment.innerHTML = _('difficulty_level_1_descr')
             var difficulty = document.createElement('select')
@@ -237,7 +420,6 @@ function mainMenu() {
                 era.setAttribute('type', 'radio')
                 era.setAttribute('name', 'era')
                 era.value = e
-                if (i == 0) era.checked = true
                 var eraLabel = document.createElement('label')
                 eraLabel.appendChild(era)
                 eraLabel.innerHTML += _(e+'_era')
@@ -313,17 +495,19 @@ function mainMenu() {
             let header = createHeader(_('start_new_game_header'))
             let button = createButton(_('start_new_game'), function() {
                 let params = {
+                    gender: selectedGender,
                     difficulty: difficulty.value,
                     era: selectedEra,
                     race: selectedRace,
                     season: season.value
                 }
-                if (params.difficulty && params.era && params.race && params.season) {
+                if (params.gender && params.difficulty && params.era && params.race && params.season) {
                     mm.remove()
                     showLoadingGame()
                     startNewGame(params)
                 } else {
                     let warnings = _('warning_not_enough_data_new_game');
+                    if (!params.gender) warnings += '\n' + _('warning_enter_gender');
                     if (!params.difficulty) warnings += '\n' + _('warning_enter_difficulty');
                     if (!params.era) warnings += '\n' + _('warning_enter_era');
                     if (!params.race) warnings += '\n' + _('warning_enter_race');
@@ -336,7 +520,7 @@ function mainMenu() {
 
             newGameBlock.appendChild(newGameStart)
             newGameBlock.appendChild(newGameParams)
-            mm.appendChild(newGameBlock)
+            mm.insertBefore(newGameBlock, mm.firstChild)
         },
         function () {
             if (loadGameList) return;
@@ -355,7 +539,47 @@ function mainMenu() {
 
             loadGameList = document.createElement('div')
             loadGameList.className = "right_main_menu_panel game_view_block"
-            mm.appendChild(loadGameList)
+
+            let header = createHeader(_('load_game_header'))
+            header.style.display = 'block'
+            let gameList = createSavedGameList(function() {
+                deleteButton.removeAttribute('disabled')
+                loadButton.removeAttribute('disabled')
+            }, function() {
+                deleteButton.removeAttribute('disabled')
+                loadButton.setAttribute('disabled', 'disabled')
+            })
+            let deleteButton = createButton(_('delete_saved_game'), function() {
+                let sure = confirm(_('sure_to_delete_saved_game'))
+                if (sure) {
+                    let filepath = selectedSavedGame.dataset.path;
+                    fs.unlink(filepath, (err) => {
+                        if (err) {
+                            console.log('Unable to delete saved game: ' + err)
+                            alert(_('error_delete_saved_game'))
+                        } else {
+                            selectedSavedGame.remove();
+                            if (gameList.children.length == 0) gameList.appendChild(emptySavedEntry())
+                            selectedSavedGame = null;
+                            deleteButton.setAttribute('disabled', 'disabled')
+                            loadButton.setAttribute('disabled', 'disabled')
+                        }
+                    });
+                }
+            }, '40%', '30px')
+            deleteButton.setAttribute('disabled', 'disabled')
+            let loadButton = createButton(_('load_saved_game'), function() {
+                //load game
+            }, '40%', '30px')
+            loadButton.setAttribute('disabled', 'disabled')
+            let headerDiv = document.createElement('div')
+            headerDiv.style.borderBottom = '1px solid white';
+            headerDiv.appendChild(header)
+            headerDiv.appendChild(deleteButton)
+            headerDiv.appendChild(loadButton)
+            loadGameList.appendChild(headerDiv)
+            loadGameList.appendChild(gameList)
+            mm.insertBefore(loadGameList, mm.firstChild)
         },
         function () {
             if (encycBlock) return;
@@ -366,6 +590,7 @@ function mainMenu() {
             if (loadGameList) {
                 loadGameList.remove();
                 loadGameList = null;
+                selectedSavedGame = null;
             }
             if (credits) {
                 credits.remove();
@@ -386,7 +611,7 @@ function mainMenu() {
             let pda = createPdaElementFromData(articleStructure)
             encycBlock.appendChild(header)
             encycBlock.appendChild(pda)
-            mm.appendChild(encycBlock)
+            mm.insertBefore(encycBlock, mm.firstChild)
         },
         function () {
             if (credits) return;
@@ -397,6 +622,7 @@ function mainMenu() {
             if (loadGameList) {
                 loadGameList.remove();
                 loadGameList = null;
+                selectedSavedGame = null;
             }
             if (encycBlock) {
                 encycBlock.remove();
@@ -409,20 +635,20 @@ function mainMenu() {
             credits.appendChild(header)
             var text = document.createElement('div')
             text.className = 'main_menu_block_under_header'
+            text.style.maxHeight = 'calc(100vh - 60px - 50px)';
             fs.readFile(path.join(__dirname, 'CREDITS.txt'), 'utf8', function(err, contents) {
-                if (err) text.innerHTML = err;
+                if (err) {
+                    console.log('Unable to read file:' + err)
+                    text.innerHTML = _('error_read_credits');
+                }
                 else text.innerHTML = contents.replace(/\n/g, "<br>");
             });
             credits.appendChild(text)
-            mm.appendChild(credits)
+            mm.insertBefore(credits, mm.firstChild)
         },
         function () {
             let sure = confirm(_('sure_to_exit'))
-            if (sure) {
-                const remote = require('electron').remote
-                let w = remote.getCurrentWindow()
-                w.close()
-            }
+            if (sure) quit();
         }
     ];
     for (let i = 0; i<mainOptions.length; i++) {
